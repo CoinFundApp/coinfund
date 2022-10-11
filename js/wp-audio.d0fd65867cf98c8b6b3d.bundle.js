dfc612a6ed473968844b200caec1383534cb9b9b
LA <61266207+Spl0itable@users.noreply.github.com>
2022-10-11 19:00:34 +1030

    Add files via upload

---
+++ js/wp-audio.d0fd65867cf98c8b6b3d.bundle.js
@@ -0,0 +1,31 @@
+/*! elementor - v3.7.8 - 02-10-2022 */
+"use strict";
+(self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["wp-audio"],{
+
+/***/ "../assets/dev/js/frontend/handlers/wp-audio.js":
+/*!******************************************************!*\
+  !*** ../assets/dev/js/frontend/handlers/wp-audio.js ***!
+  \******************************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports["default"] = void 0;
+
+class WpAudio extends elementorModules.frontend.handlers.Base {
+  onInit() {
+    super.onInit();
+    window.wp.mediaelement.initialize();
+  }
+
+}
+
+exports["default"] = WpAudio;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=wp-audio.d0fd65867cf98c8b6b3d.bundle.js.map
