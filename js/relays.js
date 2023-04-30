379c9fa1a64e16005388b76235363801b8b6d73c
LA <61266207+Spl0itable@users.noreply.github.com>
2023-04-30 12:43:35 +0930

    Update relays.js

--- js/relays.js
+++ js/relays.js
@@ -1,6 +1,4 @@
 // List of relays for campaigns data
 var relays = [
-    { name: "https://data3.coinfundit.com" },
-    { name: "https://data2.coinfundit.com" },
     { name: "https://data.coinfundit.com" },
 ];
