a48ab20f129ff04f0ead4e3638829d5d076be16c
LA <61266207+Spl0itable@users.noreply.github.com>
2023-04-30 12:06:16 +0930

    Decentralization of campaign data
    Relays defined in the relays.js file define available servers to push/pull campaign data

---
+++ js/relays.js
@@ -0,0 +1,6 @@
+// List of relays for campaigns data
+var relays = [
+    { name: "https://data3.coinfundit.com" },
+    { name: "https://data2.coinfundit.com" },
+    { name: "https://data.coinfundit.com" },
+];
