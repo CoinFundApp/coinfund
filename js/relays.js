8f8e649aa64f4e2aa67761f15ff210d008fb917e
LA <61266207+Spl0itable@users.noreply.github.com>
2023-05-07 15:10:28 +0930

    Added relay

--- js/relays.js
+++ js/relays.js
@@ -1,5 +1,6 @@
 // List of relays for campaigns data
 var relays = [
-    { name: "https://data.coinfundit.com" },
-    { name: "https://h4x0r.host" },
+    { "name": "https://data.coinfundit.com" },
+    { "name": "https://cfidata.fun" },
+    { "name": "https://h4x0r.host" }
 ];
