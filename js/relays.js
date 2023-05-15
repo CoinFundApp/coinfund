5301c57fffc58526fd40a474580593b1cf49e54d
LA <61266207+CoinFundIt@users.noreply.github.com>
2023-05-15 13:33:13 +0930

    Update relays.js

--- js/relays.js
+++ js/relays.js
@@ -1,6 +1,5 @@
 // List of relays for campaigns data
 var relays = [
-    { name: "https://data.coinfundit.com" },
-    { name: "https://cfidata.fun" },
-    { name: "https://h4x0r.host" }
+    { "name": "https://data.coinfundit.com" },
+    { "name": "https://cfidata.fun" },
 ];
